c582393f489f75dae010ac4849cf828015e6b23a
Dhanasekar16big7 <dhanasekar@big7solutions.com>
2025-01-09 17:07:05 +0530

    add team check

--- app/(web)/components/spacebar.tsx
+++ app/(web)/components/spacebar.tsx
@@ -1326,7 +1326,7 @@ const SpaceBar: React.FC<loggedUserDataProps> = ({ loggedUserData }) => {
               )
             }
             
-          {((loggedUserData?.role === "owner" && (spaceLength > 0 || spaceLength === 1) ) ||
+          {((loggedUserData?.role === "owner" && (spaceLength > 0) ) ||
             (loggedUserData?.role === "User" &&
               ((loggedUserData?.access?.team !== true &&
                 loggedUserData?.access?.all === true) ||
